ad76a8d1ae491206065755b2ff04793992722aed
nrdyelephant <48748277+nrdyelephant@users.noreply.github.com>
2020-10-21 02:45:56 -0700

    Update compile.js

--- compile.js
+++ compile.js
@@ -5,6 +5,6 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Inbox']
 
 
